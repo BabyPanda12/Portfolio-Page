79a4be1c0f099c6d8b90f82c30cece8d150d2428
Ivana Stojanova <558296@student.saxion.nl>
2025-05-13 00:58:39 +0200

    Added portfolio section

---
+++ my-portfolio/data/potrfolio_data.js
@@ -0,0 +1,19 @@
+export default [
+    {
+        title:'Complex Data Structers',
+        imgUrl:'src/assets/images/Algorithms.jpg',
+        stack:['Java','JavaDoc','unit testing'],
+        link: 'https://github.com/BabyPanda12/Complex-Data-Structures'
+    },
+    {
+        title:'Auction site',
+        imgUrl:'/src/assets/images/image.png',
+        stack:['HTML','TailwindCSS','Svelte','JavaScript'],
+        link: 'https://github.com/BabyPanda12/Auction-site'
+    },
+    {
+        title:'This Portfolio',
+        imgUrl:'/src/assets/images/pp.png',
+        stack:['HTML','TailwindCSS','JavaScript'],
+        link: 'https://github.com/BabyPanda12/Portfolio-Page'    }
+]
