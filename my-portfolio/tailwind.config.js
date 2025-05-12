8d482fa50ef7bf81d28d91e29586f331f7b4c1d3
Ivana Stojanova <558296@student.saxion.nl>
2025-05-12 15:45:09 +0200

    Initiated a new proffesional portfolio page with Vite, React, tailwind

---
+++ my-portfolio/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./index.html','./src/**/*.{jsx,js}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['inter','serif'],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
