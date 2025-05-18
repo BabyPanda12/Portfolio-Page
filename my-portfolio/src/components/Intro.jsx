1105ce6f7e75f33f840d8d5d59bc2446e45b4a58
Ivana Stojanova <558296@student.saxion.nl>
2025-05-19 00:03:41 +0200

    Fixed typos

--- my-portfolio/src/components/Intro.jsx
+++ my-portfolio/src/components/Intro.jsx
@@ -10,14 +10,14 @@ function Intro() {
             <h1 className="text-4xl md:text-7xl mb-1 bd:mb-3 font-bold">Hello from Ivana Stojanova!</h1>
             <p className="text-base md:text-x1 mb-3 font-medium">That's me - a prospective Software Engineer!
                 This is my personal portfolio website(it is still being worked on, as creativity never stops). 
-                I hope that it's fun for you to see, since being an IT student <i class="fa-solid fa-terminal"></i>  keeps you busy all the time, I didn't have much time to dedicate to this website. 
+                I hope that it's fun for you to see, since being an IT student <i class="fa-solid fa-terminal"></i>  keeps you busy all the time, this website is still a work in progress. 
                 Coding is more than just a skill to me — it’s a way to contribute to something bigger. I'm passionate about the impact technology can have on improving society, and I’m excited to be a part of that journey.<br></br>
-                Of course, life isn’t all about code! In do also find time for leiser activites, like travelling <i className="fa-solid fa-earth-americas"></i>, watching tv shows and movies, reading, cooking(and eating, I belive I live to eat, not the other way aroudn!);
+                Of course, life isn’t all about code! I do also find time for leisure activities, like travelling <i className="fa-solid fa-earth-americas"></i>, watching tv shows and movies, reading, cooking(and eating, I belive I live to eat, not the other way around!);
             </p>
             <p className="text-sm max-w-xl mb-6 font-bold"> Below you can see some of the projects I have worked on. 
-                By clicking on them you will be taken to the corresponding GitHub reposetories. 
+                By clicking on them you will be taken to the corresponding GitHub repositories. Since completing these projects, I’ve worked on more advanced projects and continue to improve my skills.
                 I know these projects are far from perfect, 
-                still, I want to show my journey and growth. Since completing these projects, I’ve worked on more advanced projects and continue to improve my skills.
+                still, I want to show my journey and growth. 
                 </p>
         </div>
     )
